bcb690f8eb7c843baebcb543d8d8c26eb7721dfd
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-07-30 18:15:18 +0200

    V.0.9.1 OwnTheSpot. Added About us, Contact and rules pages

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,16 @@
 class SiteController < ApplicationController
+
   def home
     @skater = Skater.new
   end
+
+  def rules
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'site#home'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/rules' => 'site#rules'
+  get '/contact' => 'site#contact'
+  get 'about' => 'site#about'
 
   resources :skaters
   resources :spots
