f26d9a34cc12db5620e25bd9f7edf40992e0b470
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-07-31 17:41:55 +0200

    Fixing heroku

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,6 @@
 class Vote < ActiveRecord::Base
   validates :voter_id, presence: true
   validates :video_id, presence: true
-  validates :result, presence: true, default: 0, inclusion: { in: -1..1 },
-    message: "%{value} is not a valid category" }
+  validates :result, presence: true, default: 0, inclusion: { in: -1..1 }
   belongs_to :video
 end
