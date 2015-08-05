295e23d6dc87da12dafe04341c81d2548653a656
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-08-05 17:19:19 +0200

    V.1.9 OwnTheSpot Added border between video carousels

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, :flash => {success: "You've been successfully logged out" }
+    redirect_to root_path
   end
 
 end
