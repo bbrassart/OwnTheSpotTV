0cc0067270bfec219fa357d0a5bb509c941f8f3f
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-10-17 09:38:26 +0200

    Trying to fix Materialize

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Rails.application.configure do
     }
   }
 
+  config.action_controller.relative_url_root  = "/"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
