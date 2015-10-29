8eb232de515236f99dbf9b1f1ff49822c77b402c
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-10-29 18:38:38 +0100

    Tried to fix CORS gem

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module OwnTheSpot
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
 
-    config.middleware.insert_before (0, "Rack::Cors", logger: (-> { Rails.logger }) ) do
+    config.middleware.insert_before(0, "Rack::Cors", logger: (-> { Rails.logger })) do
       allow do
         origins '*'
         resource '*',
