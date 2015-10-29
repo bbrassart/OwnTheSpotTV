250f0b8f21ed955721683d7cc7d271b0b4f9b405
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-10-29 16:41:36 +0100

    Fixed a wrong link

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module OwnTheSpot
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '/api/v1/*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
 
