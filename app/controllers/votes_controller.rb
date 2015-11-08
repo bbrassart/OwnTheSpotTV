cec0aed7fe1088e71cdb532d773a7f98b89cdb75
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-08 11:45:16 +0100

    Fixed some errors

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,7 +12,9 @@ class VotesController < ApplicationController
     @vote.result = 1
     @vote.video_id = params[:video_id]
     video = @vote.video
-    if @vote.valid? && @vote.voter_id != Video.find(params[:video_id].skater_id)
+    if @vote.voter_id == Video.find(params[:video_id].skater_id)
+      redirect_to spot_path(video.spot), flash: {danger: "Your vote couldn't be processed" }
+    elsif @vote.valid?
       @vote.save
       redirect_to spot_path(video.spot), flash: {success: "Your vote has been registered!"}
     else
