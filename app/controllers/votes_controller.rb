cc1b9ce7cd2e09014649d36a24c69a6c99873754
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-08 11:53:15 +0100

    Fixing voting system

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,34 +7,34 @@ class VotesController < ApplicationController
 
   def like
     @votes = Vote.all
+    @video = Video.find(params[:video_id])
     @vote = Vote.new
     @vote.voter_id = current_user.id
     @vote.result = 1
-    @vote.video_id = params[:video_id]
-    video = @vote.video
-    if @vote.voter_id == Video.find(params[:video_id].skater_id)
-      redirect_to spot_path(video.spot), flash: {danger: "Oops... Your vote couldn't be saved" }
+    @vote.video_id = @video.id
+    if @vote.voter_id == @video.skater_id
+      redirect_to spot_path(@video.spot_id), flash: {danger: "Oops... Your vote couldn't be saved" }
     elsif @vote.valid?
       @vote.save
-      redirect_to spot_path(video.spot), flash: {success: "Your vote has been registered!"}
+      redirect_to spot_path(@video.spot_id), flash: {success: "Your vote has been registered!"}
     else
-      redirect_to spot_path(video.spot), flash: {danger: "Oops... Your vote couldn't be saved" }
+      redirect_to spot_path(@video.spot_id), flash: {danger: "Oops... Your vote couldn't be saved" }
     end
   end
 
   def dislike
     @vote = Vote.new
+    @video = Video.find(params[:video_id])
     @vote.voter_id = current_user.id
     @vote.result = -1
-    @vote.video_id = params[:video_id]
-    video = @vote.video
-    if @vote.voter_id == Video.find(params[:video_id].skater_id)
-      redirect_to spot_path(video.spot), flash: {danger: "Oops... Your vote couldn't be saved" }
+    @vote.video_id = @video.id
+    if @vote.voter_id == @video.skater_id
+      redirect_to spot_path(@video.spot_id), flash: {danger: "Oops... Your vote couldn't be saved" }
     elsif @vote.valid?
       @vote.save
-      redirect_to spot_path(video.spot), flash: {success: "Your vote has been registered!"}
+      redirect_to spot_path(@video.spot_id), flash: {success: "Your vote has been registered!"}
     else
-      redirect_to spot_path(video.spot), flash: {danger: "Oops... Your vote couldn't be saved" }
+      redirect_to spot_path(@video.spot_id), flash: {danger: "Oops... Your vote couldn't be saved" }
     end
   end
 end
