bc5ee5ad00459339541973f66ad40ba729051d57
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-10 00:04:10 +0100

    Fixing insta sign in

--- app/controllers/instagrams_controller.rb
+++ app/controllers/instagrams_controller.rb
@@ -1,13 +1,13 @@
 require 'instagram'
 class InstagramsController < ApplicationController
-
+  CALLBACK_URL = "http://ownthespot.tv/oauth/callback"
 
   def redirection
-    redirect_to Instagram.authorize_url(:redirect_uri => "http://ownthespot.tv/oauth/callback")
+    redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
   end
 
   def callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => "http://ownthespot.tv/oauth/callback")
+    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
     current_user.update_columns(
       access_token: response.access_token
     )
