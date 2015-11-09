c7e1f828f7ee983e4b4f9c9ab37e30fb2f0dafad
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-10 00:55:23 +0100

    Added button on profile page to see all the links to the media

---
+++ app/assets/javascripts/instafeed.js
@@ -0,0 +1,23 @@
+var instaFeed = function() {
+  this.url = "https://api.instagram.com/v1/users/self/feed?access_token=";
+  this.setInitialListeners();
+}
+
+instaFeed.prototype.setInitialListeners = function() {
+  $('.container').on('click', '#feed_launch', function(event) {
+    debugger;
+    identifier = $(this).data["id"]
+    this.processFeed(identifier).bind(this);
+  });
+}
+
+instaFeed.prototype.processFeed = function(identifier) {
+  var request = $.get(this.url + identitfier);
+  request.done(function(response) {
+    response[0].body["data"].forEach(function(media) {
+      if (media["type"] == "video") {
+        $('#display_feed').append(`<li>${media["link"]}</li>`);
+      };
+    });
+  })
+}
