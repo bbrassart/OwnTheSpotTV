dc697d3b6a93ad468a26034cd9c3031b1e6d22a9
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-12 10:06:22 +0100

    Turned a coffee into a regular js

--- app/assets/javascripts/spots.js
+++ app/assets/javascripts/spots.js
@@ -1,16 +1,14 @@
 
 var SpotDropdown = function() {
-  this.spotButton = $("#show_spot_info-button");
-  this.skaterButton = $("#show_skater_info-button");
   this.init();
 }
 
 SpotDropdown.prototype.init = function () {
   $("#show_spot_info").on('click', function() {
-    this.spotButton.fadeToggle("fast");
+    $("#show_spot_info-button").fadeToggle("fast");
   });
 
   $("#show_skater_info").on('click', function() {
-    this.skaterButton.fadeToggle("fast");
+    $("#show_skater_info-button").fadeToggle("fast");
   });
 }
