8296f1737e59f5af8e68b778962f0bc9e539e275
Baptiste Brassart <brassart.baptiste@gmail.com>
2015-11-15 00:53:53 +0100

    Changed dropdown menus

---
+++ app/assets/javascripts/spots.js
@@ -0,0 +1,10 @@
+function listenToDropDown(card, arrow) {
+  var oddClick = true;
+  card.click(function() {
+    card.animate({
+        height: oddClick ? '-=55' : '+=55'
+    },500);
+    oddClick = !oddClick;
+    arrow.toggleClass('invisible--arrow');
+  })
+};
