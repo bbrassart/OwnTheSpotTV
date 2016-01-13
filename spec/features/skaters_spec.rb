22d450f2b0591d85a5c354aed6410a0c20735b9d
Baptiste Brassart <brassart.baptiste@gmail.com>
2016-01-13 10:31:39 +0100

    Improved feature testing

--- spec/features/skaters_spec.rb
+++ spec/features/skaters_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 RSpec.feature "Homepage", :type => :feature do
   describe "User visits homepage" do
-    it "sees the homepage correctly" do
+    it "displays the homepage correctly" do
       visit "/"
       expect(page).to have_text("Barcelona Skateboarding")
     end
-
   end
+end
 
+RSpec.feature "Leaderboard", :type => :feature do
   describe "User visits leaderboard" do
-    it "sees the leaderboard correctly" do
-      visit "/rankings"
+    it "displays the leaderboard correctly" do
+      visit "/"
+      find("#nav-mobile").click_link "Leaderboard"
       expect(page).to have_text("Top 5 ranking")
     end
   end
+end
 
+RSpec.feature "Login", :type => :feature do
   describe "User logs in" do
     context 'matching case' do
       it "validates login" do
@@ -25,7 +29,8 @@ RSpec.feature "Homepage", :type => :feature do
           password_confirmation: "veryStrongPass123",
           stance: "goofy")
         skater.valid?
-        visit "/login"
+        visit "/"
+        find("#nav-mobile").click_link "Log in"
         fill_in 'Username', :with => 'Pete'
         fill_in 'Password', :with => "veryStrongPass123"
         find('#loginForm').click_button "Log in"
@@ -34,15 +39,28 @@ RSpec.feature "Homepage", :type => :feature do
     end
 
     context 'not matching cases' do
-      it "won't validate login" do
+      before do
         skater = Skater.create(username: "Pete",
           email: "pete@example.com",
           password: "veryStrongPass123",
           password_confirmation: "veryStrongPass123",
           stance: "goofy")
         skater.valid?
-        visit "/login"
+      end
+
+      it "won't validate login because of an error in password" do
+        visit "/"
+        find("#nav-mobile").click_link "Log in"
         fill_in 'Username', :with => 'Peter'
+        fill_in 'Password', :with => "veryStrongPass123"
+        find('#loginForm').click_button "Log in"
+        expect(page.current_path).to eq '/'
+      end
+
+      it "won't validate login because username remained empty" do
+        visit "/"
+        find("#nav-mobile").click_link "Log in"
+        fill_in 'Username', :with => ''
         fill_in 'Password', :with => "veryStrongPass123"
         find('#loginForm').click_button "Log in"
         expect(page.current_path).to eq '/'
